4be76cf09a0142e4420a8be2fdb5762ba960d183
Ángel <33127189+anthares101@users.noreply.github.com>
2020-12-02 12:14:23 +0100

    Add necessary coloqio resources

--- terraform/coloqio.tf
+++ terraform/coloqio.tf
@@ -15,11 +15,37 @@ resource "aws_ecr_lifecycle_policy" "ecr_coloqio_frontend" {
     "rules": [
         {
             "rulePriority": 1,
-            "description": "Keep last 20 images",
+            "description": "Keep last 20 release images",
+            "selection": {
+                "tagStatus": "tagged",
+                "tagPrefixList": ["v"],
+                "countType": "imageCountMoreThan",
+                "countNumber": 20
+            },
+            "action": {
+                "type": "expire"
+            }
+        },
+        {
+            "rulePriority": 2,
+            "description": "Keep last 10 staging images",
+            "selection": {
+                "tagStatus": "tagged",
+                "tagPrefixList": ["staging"],
+                "countType": "imageCountMoreThan",
+                "countNumber": 10
+            },
+            "action": {
+                "type": "expire"
+            }
+        },
+        {
+            "rulePriority": 3,
+            "description": "Only keep review apps images for 7 days",
             "selection": {
                 "tagStatus": "any",
                 "countType": "imageCountMoreThan",
-                "countNumber": 20
+                "countNumber": 7
             },
             "action": {
                 "type": "expire"
@@ -33,4 +59,66 @@ EOF
 
 output "coloqio-frontend-ecr" {
   value = aws_ecr_repository.coloqio_frontend.repository_url
+}
+
+resource "aws_ecr_repository" "coloqio_backend" {
+  name                 = "coloqio-backend"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "ecr_coloqio_backend" {
+  repository = aws_ecr_repository.coloqio_backend.name
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Keep last 20 release images",
+            "selection": {
+                "tagStatus": "tagged",
+                "tagPrefixList": ["v"],
+                "countType": "imageCountMoreThan",
+                "countNumber": 20
+            },
+            "action": {
+                "type": "expire"
+            }
+        },
+        {
+            "rulePriority": 2,
+            "description": "Keep last 10 staging images",
+            "selection": {
+                "tagStatus": "tagged",
+                "tagPrefixList": ["staging"],
+                "countType": "imageCountMoreThan",
+                "countNumber": 10
+            },
+            "action": {
+                "type": "expire"
+            }
+        },
+        {
+            "rulePriority": 3,
+            "description": "Only keep review apps images for 7 days",
+            "selection": {
+                "tagStatus": "any",
+                "countType": "imageCountMoreThan",
+                "countNumber": 7
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
+
+output "coloqio-backend-ecr" {
+  value = aws_ecr_repository.coloqio_backend.repository_url
 }
